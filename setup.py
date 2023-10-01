7b2baa391c5e9b60abf76f2cff494d1012bbcea1
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2023-10-01 20:52:58 +0200

    add types

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup
 
-setup()
+setup(
+    packages=["xbmc", "xbmcaddon", "xbmcdrm", "xbmcgui", "xbmcplugin", "xbmcvfs"],
+    package_data={
+        "xbmc": ["py.typed"],
+        "xbmcaddon": ["py.typed"],
+        "xbmcdrm": ["py.typed"],
+        "xbmcgui": ["py.typed"],
+        "xbmcplugin": ["py.typed"],
+        "xbmcvfs": ["py.typed"],
+    }
+)
